f70cbbcfa88958dc0eaf7f714fc7880ba85c5b90
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2024-05-07 02:08:17 +0530

    indentation corrected

--- app/api/paymentverify/route.js
+++ app/api/paymentverify/route.js
@@ -5,44 +5,44 @@ import crypto from "crypto";
 // import Payment from "../../../database/model/Payment"
 // import dbConnect from '../../../database/database'
 const instance = new Razorpay({
-    key_id: process.env.RAZORPAY_API_KEY,
-    key_secret: process.env.RAZORPAY_APT_SECRET,
-  });
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_APT_SECRET,
+});
 
-export async function POST(req,res) {
+export async function POST(req, res) {
 
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-  await req.json();
-   const body = razorpay_order_id + "|" + razorpay_payment_id;
-console.log("id==",body)
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    await req.json();
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+  console.log("id==", body)
 
- const expectedSignature = crypto
-   .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
-   .update(body.toString())
-   .digest("hex");
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
+    .update(body.toString())
+    .digest("hex");
 
-const isAuthentic = expectedSignature === razorpay_signature;
+  const isAuthentic = expectedSignature === razorpay_signature;
 
 
- if (isAuthentic) {
+  if (isAuthentic) {
 
-  console.log(Payment)
+    // console.log(Payment)
 
-  
 
-  //  return NextResponse.redirect(new URL('/paymentsuccess', req.url));
 
-} else {
+    return NextResponse.redirect(new URL('/', req.url));
+
+  } else {
     return NextResponse.json({
-        message: "fail"
-      }, {
-        status: 400,
-      })
+      message: "fail"
+    }, {
+      status: 400,
+    })
 
-}
+  }
 
 
-return NextResponse.json({
+  return NextResponse.json({
     message: "success"
   }, {
     status: 200,
