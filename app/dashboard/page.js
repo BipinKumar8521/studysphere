aa921aaf00f146c4d2b696631a75cc3be4bfa1a3
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2024-05-08 02:54:58 +0530

    design of button changed

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,5 @@
 // pages/index.js
 
-import Head from 'next/head';
 import "./styles.css"
 import { UserButton } from '@clerk/nextjs';
 
@@ -26,32 +25,32 @@ export default function Home() {
                     {/* HTML Course */}
                     <div className="course">
                         {/* unsplash img for html */}
-                        <img src="https://source.unsplash.com/300x200/?html, code" alt="HTML Course Image" />
-                        <h2>HTML Basics</h2>
+                        <img src="https://source.unsplash.com/300x200/?physics, code" alt="Physics Course Image" />
+                        <h2>Physics</h2>
                         <p>Learn the fundamentals of HTML including tags, attributes, and semantic markup.</p>
                         <div className="course-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
                             <div><p>Price: $50</p> <p>Instructor: Asish Sir</p> </div>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button className="add-to-cart" style={{ backgroundColor: 'green', color: 'white', height: '30px', width: '100px', borderRadius: '5px', cursor: 'pointer' }}>Add to Cart</button></div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button class="btn-5"><span>Buy Now</span></button></div>
                         </div>
                     </div>
                     {/* CSS Course */}
                     <div className="course">
-                        <img src="https://source.unsplash.com/300x200/?css, code" alt="CSS Course Image" />
-                        <h2>CSS Styling</h2>
+                        <img src="https://source.unsplash.com/300x200/?chemistry, code" alt="Chemistry Course Image" />
+                        <h2>Chemistry</h2>
                         <p>Master CSS styling techniques such as selectors, box model, flexbox, and grid.</p>
                         <div className="course-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
                             <div><p>Price: $50</p> <p>Instructor: Asish Sir</p> </div>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button className="add-to-cart" style={{ backgroundColor: 'green', color: 'white', height: '30px', width: '100px', borderRadius: '5px', cursor: 'pointer' }}>Add to Cart</button></div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button class="btn-5"><span>Buy Now</span></button></div>
                         </div>
                     </div>
                     {/* JavaScript Course */}
                     <div className="course">
-                        <img src="https://source.unsplash.com/300x200/?javascript, code" alt="JavaScript Course Image" />
-                        <h2>JavaScript Fundamentals</h2>
+                        <img src="https://source.unsplash.com/300x200/?mathematics, code" alt="Mathematics Course Image" />
+                        <h2>Mathematics</h2>
                         <p>Explore the core concepts of JavaScript including variables, functions, and loops.</p>
                         <div className="course-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
                             <div><p>Price: $50</p> <p>Instructor: Asish Sir</p> </div>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button className="add-to-cart" style={{ backgroundColor: 'green', color: 'white', height: '30px', width: '100px', borderRadius: '5px', cursor: 'pointer' }}>Add to Cart</button></div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button class="btn-5"><span>Buy Now</span></button></div>
                         </div>
                     </div>
                     {/* React Course */}
@@ -61,7 +60,7 @@ export default function Home() {
                         <p>Build dynamic web applications with React.js, including state management.</p>
                         <div className="course-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
                             <div><p>Price: $50</p> <p>Instructor: Asish Sir</p> </div>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button className="add-to-cart" style={{ backgroundColor: 'green', color: 'white', height: '30px', width: '100px', borderRadius: '5px', cursor: 'pointer' }}>Add to Cart</button></div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button class="btn-5"><span>Buy Now</span></button></div>
                         </div>
 
                     </div>
@@ -72,7 +71,7 @@ export default function Home() {
                         <p>Discover the power of Next.js for server-side rendering, routing, and API integration.</p>
                         <div className="course-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
                             <div><p>Price: $50</p> <p>Instructor: Asish Sir</p> </div>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button className="add-to-cart" style={{ backgroundColor: 'green', color: 'white', height: '30px', width: '100px', borderRadius: '5px', cursor: 'pointer' }}>Add to Cart</button></div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button class="btn-5"><span>Buy Now</span></button></div>
                         </div>
                     </div>
                     {/* TypeScript Course */}
@@ -82,7 +81,7 @@ export default function Home() {
                         <p>Learn TypeScript to enhance the development of scalable web applications.</p>
                         <div className="course-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
                             <div><p>Price: $50</p> <p>Instructor: Asish Sir</p> </div>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button className="add-to-cart" style={{ backgroundColor: 'green', color: 'white', height: '30px', width: '100px', borderRadius: '5px', cursor: 'pointer' }}>Add to Cart</button></div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}><button class="btn-5"><span>Buy Now</span></button></div>
                         </div>
                     </div>
                 </div>
