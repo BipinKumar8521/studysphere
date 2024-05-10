9f5a69c7b1a8a5d538912af01c0ea8414c175b6f
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2024-05-10 23:47:09 +0530

    teacher dashboard created

--- app/decide/page.js
+++ app/decide/page.js
@@ -20,45 +20,52 @@ export default async function Home() {
   });
 
   return (
-    <div className="container_decide">
-      <div className="cards_decide">
-        <div className="imgBx_decide">
-          {/* <img src=" https://www.shutterstock.com/image-vector/cute-male-teacher-cartoon-character-260nw-2342219157.jpg" /> */}
-          <Image src="/images/30.png" alt="Logo" width={150} height={150} />
+    <>
+      <header className="landing-page">
+        <div className="container-home">
+          <h1>Which role best describes you?</h1>
         </div>
-        <div className="content_decide">
-          <div className="details_decide">
-            <h2>
-              <span>
-                Welcome, esteemed educator! As a teacher, you hold the power to
-                inspire, educate, and shape the minds of tomorrow.
-              </span>
-            </h2>
-            <Link href="/registration-form/2">
-              <button className="designful-button">Join as a Teacher</button>
-            </Link>
+      </header>
+      <div className="container_decide">
+        <div className="cards_decide">
+          <div className="imgBx_decide">
+            {/* <img src=" https://www.shutterstock.com/image-vector/cute-male-teacher-cartoon-character-260nw-2342219157.jpg" /> */}
+            <Image src="/images/30.png" alt="Logo" width={150} height={150} />
+          </div>
+          <div className="content_decide">
+            <div className="details_decide">
+              <h2>
+                <span>
+                  Welcome, esteemed educator! As a teacher, you hold the power to
+                  inspire, educate, and shape the minds of tomorrow.
+                </span>
+              </h2>
+              <Link href="/registration-form/2">
+                <button className="designful-button">Join as a Teacher</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="cards_decide">
-        <div className="imgBx_decide">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-9hjHUZvGyX8GT4ly3b168-S0EYr8tdvvA2ZV2MwhQ&s" />
-        </div>
-        <div className="content_decide">
-          <div className="details_decide">
-            <h2>
-              <span>
-                Unlock the doors to knowledge and endless possibilities as you
-                embark on your educational journey
-              </span>
-            </h2>
-            <Link href="/registration-form/1">
-              <button className="designful-button">Join as a Student</button>
-            </Link>
+        <div className="cards_decide">
+          <div className="imgBx_decide">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-9hjHUZvGyX8GT4ly3b168-S0EYr8tdvvA2ZV2MwhQ&s" />
+          </div>
+          <div className="content_decide">
+            <div className="details_decide">
+              <h2>
+                <span>
+                  Unlock the doors to knowledge and endless possibilities as you
+                  embark on your educational journey
+                </span>
+              </h2>
+              <Link href="/registration-form/1">
+                <button className="designful-button">Join as a Student</button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
