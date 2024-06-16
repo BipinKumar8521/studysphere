fe463553f5d0d1dda9e84231555964a48f77dd33
Bipin <bk3284488@gmail.com>
2024-06-16 12:29:49 +0530

    pages removed

--- app/(pages)/dashboard/page.js
+++ app/(pages)/dashboard/page.js
@@ -4,67 +4,62 @@ import userProfileCompleted from "@/app/utils/userProfileCompleted";
 import { redirect } from "next/navigation";
 import CourseCard from "../../Components/Card_comp";
 
-
 export default async function Home() {
   const isUser = await userProfileCompleted();
   if (!isUser) {
     return redirect("/decide");
   }
 
-
   const courses = [
     {
       image: "https://source.unsplash.com/300x200/?physics, code",
       subject: "Physics",
-      description: "Learn the fundamentals of HTML including tags, attributes, and semantic markup.",
+      description:
+        "Learn the fundamentals of HTML including tags, attributes, and semantic markup.",
       price: 50,
-      instructor: "Asish Sir"
+      instructor: "Asish Sir",
     },
     {
       image: "https://source.unsplash.com/300x200/?chemistry, code",
       subject: "Chemistry",
-      description: "Master CSS styling techniques such as selectors, box model, flexbox, and grid.",
+      description:
+        "Master CSS styling techniques such as selectors, box model, flexbox, and grid.",
       price: 50,
-      instructor: "Asish Sir"
+      instructor: "Asish Sir",
     },
     {
       image: "https://source.unsplash.com/300x200/?mathematics, code",
       subject: "Mathematics",
-      description: "Explore the core concepts of JavaScript including variables, functions, and loops.",
+      description:
+        "Explore the core concepts of JavaScript including variables, functions, and loops.",
       price: 50,
-      instructor: "Asish Sir"
+      instructor: "Asish Sir",
     },
     {
       image: "https://source.unsplash.com/300x200/?react, code",
       subject: "React Development",
-      description: "Build dynamic web applications with React.js, including state management.",
+      description:
+        "Build dynamic web applications with React.js, including state management.",
       price: 50,
-      instructor: "Asish Sir"
+      instructor: "Asish Sir",
     },
     {
       image: "https://source.unsplash.com/300x200/?next.js, code",
       subject: "Next.js Essentials",
-      description: "Discover the power of Next.js for server-side rendering, routing, and API integration.",
+      description:
+        "Discover the power of Next.js for server-side rendering, routing, and API integration.",
       price: 50,
-      instructor: "Asish Sir"
+      instructor: "Asish Sir",
     },
     {
       image: "https://source.unsplash.com/300x200/?typescript, code",
       subject: "TypeScript",
-      description: "Learn TypeScript to enhance the development of scalable web applications.",
+      description:
+        "Learn TypeScript to enhance the development of scalable web applications.",
       price: 50,
-      instructor: "Asish Sir"
-    }
+      instructor: "Asish Sir",
+    },
   ];
-
-
-
-
-
-
-
-
-
 
   return (
     <div>
