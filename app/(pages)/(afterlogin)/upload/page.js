1278ff2e9736e30195ae7c354e33da36e75951df
Bipin <bk3284488@gmail.com>
2024-08-23 17:51:07 +0530

    certificate generation error solved

--- app/(pages)/(afterlogin)/upload/page.js
+++ app/(pages)/(afterlogin)/upload/page.js
@@ -1,111 +1,120 @@
 "use server";
 
-import "./styles.css"
+import "./styles.css";
 
 const Page = async () => {
-
-    const handleSubmit = async (formData) => {
-        "use server";
-        const data = {
-            name: formData.get('name'),
-            courseName: formData.get('courseName'),
-            price: formData.get('price'),
-            duration: formData.get('duration'),
-            role: formData.get('role'),
-            description: formData.get('description'),
-        };
-
-        console.log(data);
-        const uploadData = async () => {
-            try {
-                const response = await fetch('http://localhost:3000/api/course', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                });
-
-                if (response.ok) {
-                    console.log("Data successfully uploaded");
-                    // Handle success
-                } else {
-                    console.error("Failed to upload data");
-                    // Handle error
-                }
-            } catch (error) {
-                console.error("Error during data upload", error);
-                // Handle error
-            }
-        };
-        uploadData();
+  const handleSubmit = async (formData) => {
+    "use server";
+    const data = {
+      name: formData.get("name"),
+      courseName: formData.get("courseName"),
+      price: formData.get("price"),
+      duration: formData.get("duration"),
+      role: formData.get("role"),
+      description: formData.get("description"),
     };
 
-    return (
-        <div className="container">
-            <header>
-                <h1 id="title">Upload Course Form</h1>
-            </header>
-            <main>
-                <form id="survey-form" action={handleSubmit} method="post">
-                    <label id="name-label" htmlFor="name">
-                        Name
-                        <input
-                            id="name"
-                            name="name"
-                            type="text"
-                            placeholder="Enter your Name"
-                            required
-                        />
-                    </label>
-                    <label id="Course-label" htmlFor="courseName">
-                        Course Name
-                        <input
-                            id="courseName"
-                            name="courseName"
-                            type="text"
-                            placeholder="Enter your Course Name"
-                            required
-                        />
-                    </label>
-                    <label id="number-label" htmlFor="price">
-                        Price
-                        <input
-                            id="price"
-                            name="price"
-                            type="number"
-                            placeholder="Enter the price of course"
-                        />
-                    </label>
-                    <label id="number-label-1" htmlFor="duration">
-                        Duration of the Course
-                        <input
-                            id="duration"
-                            name="duration"
-                            type="text"
-                            placeholder="Enter the duration of course"
-                        />
-                    </label>
-                    <label htmlFor="role">
-                        Which option best describes your current Course?
-                        <select id="role" name="role" required>
-                            <option value="" disabled selected>Select Standard</option>
-                            <option value="NEET">NEET</option>
-                            <option value="JEE Main">JEE Main</option>
-                            <option value="JEE Advanced">JEE Advanced</option>
-                            <option value="12th Board">12th Board</option>
-                        </select>
-                    </label>
+    console.log(data);
+    const uploadData = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/course`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          }
+        );
 
-                    <label htmlFor="description">
-                        Description of your Course
-                        <textarea id="description" name="description" placeholder="Enter your Course description here..." rows="7"></textarea>
-                    </label>
-                    <button id="submit">Submit</button>
-                </form>
-            </main>
-        </div>
-    );
+        if (response.ok) {
+          console.log("Data successfully uploaded");
+          // Handle success
+        } else {
+          console.error("Failed to upload data");
+          // Handle error
+        }
+      } catch (error) {
+        console.error("Error during data upload", error);
+        // Handle error
+      }
+    };
+    uploadData();
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1 id="title">Upload Course Form</h1>
+      </header>
+      <main>
+        <form id="survey-form" action={handleSubmit} method="post">
+          <label id="name-label" htmlFor="name">
+            Name
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Enter your Name"
+              required
+            />
+          </label>
+          <label id="Course-label" htmlFor="courseName">
+            Course Name
+            <input
+              id="courseName"
+              name="courseName"
+              type="text"
+              placeholder="Enter your Course Name"
+              required
+            />
+          </label>
+          <label id="number-label" htmlFor="price">
+            Price
+            <input
+              id="price"
+              name="price"
+              type="number"
+              placeholder="Enter the price of course"
+            />
+          </label>
+          <label id="number-label-1" htmlFor="duration">
+            Duration of the Course
+            <input
+              id="duration"
+              name="duration"
+              type="text"
+              placeholder="Enter the duration of course"
+            />
+          </label>
+          <label htmlFor="role">
+            Which option best describes your current Course?
+            <select id="role" name="role" required>
+              <option value="" disabled selected>
+                Select Standard
+              </option>
+              <option value="NEET">NEET</option>
+              <option value="JEE Main">JEE Main</option>
+              <option value="JEE Advanced">JEE Advanced</option>
+              <option value="12th Board">12th Board</option>
+            </select>
+          </label>
+
+          <label htmlFor="description">
+            Description of your Course
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Enter your Course description here..."
+              rows="7"
+            ></textarea>
+          </label>
+          <button id="submit">Submit</button>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default Page;
--- app/utils/generateCertificateVerificationLink.js
+++ app/utils/generateCertificateVerificationLink.js
@@ -2,5 +2,5 @@ const { v4: uuidv4 } = require("uuid");
 
 export default async function generateCertificateVerificationLink() {
   const uuid = uuidv4();
-  return `${process.env.NEXT_PUBLIC_API_URL}/${uuid}`;
+  return `${process.env.NEXT_PUBLIC_URL}/verify/${uuid}`;
 }
